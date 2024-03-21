d65cded562824c7b0f84d04aad71144eeb980b16
sosoeeee <809953371@qq.com>
2024-03-21 10:15:54 +0800

    debug human traj

--- my_project/scripts/dataPrint.py
+++ my_project/scripts/dataPrint.py
@@ -77,9 +77,9 @@ class ForcePrinter:
         # 设置图窗窗口大小
 
         # 设置x名称
-        plt.xlabel('time (s)')
+        # plt.xlabel('time (s)')
         # 设置y名称
-        plt.ylabel('force (N)')
+        # plt.ylabel('force (N)')
 
         self.controllerFreq = 10
         self.t = np.arange(0, self.totalLength/self.controllerFreq, 1/self.controllerFreq)
@@ -87,8 +87,8 @@ class ForcePrinter:
     def loadForce(self, force, color, Label, Linestyle='-'):
         print(force.shape)
 
-        # if force.shape[1] == 2:
-        #     force = np.sqrt(force[:, 0] ** 2 + force[:, 1] ** 2)
+        if force.shape[1] == 2:
+            force = np.sqrt(force[:, 0] ** 2 + force[:, 1] ** 2)
 
         # 绘制轨迹并设置粗细, 设置绘制在第二幅图
         plt.plot(self.t, force[:self.totalLength], color, linewidth=1, label=Label, linestyle=Linestyle)
@@ -293,146 +293,176 @@ if __name__ == "__main__":
     # trajectoryPrinter.save("replan-3")
 
     #=========================================================# # PSI测试
-    # human = 7
-    # initZ = 0.20458
+    human = 7
+    initZ = 0.20458
     # trajectoryPrinter = TrajectoryPrinter(initZ)
 
-    # intrestingPointSet = np.array([[-0.57, -0.005]])
+    intrestingPointSet = np.array([[-0.57, -0.005]])
     # trajectoryPrinter.loadIntrestingPoint(intrestingPointSet)
 
-    # startIndex = 100
-    # endIndex = 300
+    startIndex = 100
+    endIndex = 300
 
-    # oriTrajectory = np.loadtxt("my_project\scripts\oriTraj.txt")
+    oriTrajectory = np.loadtxt("my_project\scripts\oriTraj.txt")
     # trajectoryPrinter.loadTrajectory(oriTrajectory, 'original','k', '-', endIndex, startIndex)
 
-    # ConstantPSI = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-realTraj-1.txt")
+    ConstantPSI = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-realTraj-1.txt")
     # trajectoryPrinter.loadTrajectory(ConstantPSI, 'Constant PSI', '#8f17da','--', endIndex, startIndex)
 
-    # variablePSI = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-realTraj-2.txt")
+    variablePSI = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-realTraj-2.txt")
     # trajectoryPrinter.loadTrajectory(variablePSI, 'Variable PSI', '#62da17','--', endIndex, startIndex)
 
-    # #  绘制障碍物
+    #  绘制障碍物
     # obstacle = np.loadtxt("my_project\scripts\Data\obstacle_PSI" + str(0))
     # trajectoryPrinter.loadObstacle(obstacle)
 
-    # # 设置label位置
+    # 设置label位置
     # plt.legend(loc='upper left')
 
-    # # trajectoryPrinter.print()
+    # trajectoryPrinter.print()
     # trajectoryPrinter.save("PSITrajectory")
 
-    # # 绘制力曲线
-    # forcePrinter = ForcePrinter(endIndex - startIndex - 1, 3.7)
+    # 绘制力曲线
+    forcePrinter = ForcePrinter(endIndex - startIndex - 1, 3.7)
 
-    # plt.subplot(311)
+    plt.subplot(311)
 
-    # force_ConstantPSI = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-forceSet-" + str(1) + ".txt")
-    # force_ConstantPSI = force_ConstantPSI[startIndex:endIndex]
-    # forcePrinter.loadForce(force_ConstantPSI, '#8f17da', 'Constant PSI')
+    plt.subplots_adjust(bottom=0.15)
+    plt.subplots_adjust(hspace=0.55)
 
-    # force_variablePSI = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-forceSet-" + str(2) + ".txt")
-    # force_variablePSI = force_variablePSI[startIndex:endIndex]
-    # forcePrinter.loadForce(force_variablePSI, '#62da17', 'Variable PSI')
+    force_ConstantPSI = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-forceSet-" + str(1) + ".txt")
+    force_ConstantPSI = force_ConstantPSI[startIndex:endIndex]
+    forcePrinter.loadForce(force_ConstantPSI, '#8f17da', 'Constant PSI')
 
-    # forcePrinter.loadThreshold()
+    force_variablePSI = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-forceSet-" + str(2) + ".txt")
+    force_variablePSI = force_variablePSI[startIndex:endIndex]
+    forcePrinter.loadForce(force_variablePSI, '#62da17', 'Variable PSI')
 
-    # # 设置label位置
+    forcePrinter.loadThreshold()
+
+    # 设置label位置
     # plt.legend(loc='lower right')
-    # plt.ylim(-0.5, 4.5)
-    # plt.ylabel('force (N)')
+    # 不设置label
+    plt.legend().set_visible(False)
 
-    # # forcePrinter.print()
+    plt.ylim(-0.5, 5)
+    plt.ylabel('force (N)')
+    plt.xlim(-0.5, 20.5)
 
-    # totalLength = endIndex - startIndex 
-    # controllerFreq = 10
-    # t = np.arange(0, totalLength/controllerFreq, 1/controllerFreq)
+    # 图表下方插入(a)
+    plt.text(10, -2.7, '(a)', horizontalalignment='center', verticalalignment='center', fontsize=11)
 
-    # # 绘制轨迹偏离曲线
-    # plt.subplot(312)
-    # oriTrajectory = oriTrajectory[:, startIndex:endIndex]
-    # ConstantPSI = ConstantPSI[startIndex:endIndex, :]
-    # variablePSI = variablePSI[startIndex:endIndex, :]
+    # forcePrinter.print()
 
-    # dif_ConstantPSI = getTrajectoryDifferenceALL(oriTrajectory, ConstantPSI.T)
-    # dif_variablePSI = getTrajectoryDifferenceALL(oriTrajectory, variablePSI.T)
+    totalLength = endIndex - startIndex 
+    controllerFreq = 10
+    t = np.arange(0, totalLength/controllerFreq, 1/controllerFreq)
 
-    # plt.plot(t, dif_ConstantPSI, '#8f17da', linewidth=1, label='Constant PSI', linestyle='-')
-    # plt.plot(t, dif_variablePSI, '#62da17', linewidth=1, label='Variable PSI', linestyle='-')
+    # 绘制轨迹偏离曲线
+    plt.subplot(312)
+    oriTrajectory = oriTrajectory[:, startIndex:endIndex]
+    ConstantPSI = ConstantPSI[startIndex:endIndex, :]
+    variablePSI = variablePSI[startIndex:endIndex, :]
 
-    # # 设置label位置
+    dif_ConstantPSI = getTrajectoryDifferenceALL(oriTrajectory, ConstantPSI.T)
+    dif_variablePSI = getTrajectoryDifferenceALL(oriTrajectory, variablePSI.T)
+
+    plt.plot(t, dif_ConstantPSI * 1000, '#8f17da', linewidth=1, label='Constant PSI', linestyle='-')
+    plt.plot(t, dif_variablePSI * 1000, '#62da17', linewidth=1, label='Variable PSI', linestyle='-')
+
+    # 设置label位置
     # plt.legend(loc='upper left')
-    # plt.ylabel('error (m)')
-    # # plt.show()
+    plt.ylabel('deviation (mm)')
+    plt.ylim(-5, 105)
+    plt.yticks(np.arange(0, 100.5, 50))
+    plt.xlim(-0.5, 20.5)
+    # plt.show()
 
+    plt.text(10, -50.5, '(b)', horizontalalignment='center', verticalalignment='center', fontsize=11)
 
-    # # 绘制PSI曲线
-    # plt.subplot(313)
+    # 绘制PSI曲线
+    plt.subplot(313)
 
-    # PSI_variablePSI = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-PSISet-" + str(2) + ".txt")
-    # PSI_variablePSI = PSI_variablePSI[startIndex:endIndex]
-    # plt.plot(t, PSI_variablePSI, '#62da17', linewidth=1, label='Variable PSI', linestyle='-')
+    PSI_variablePSI = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-PSISet-" + str(2) + ".txt")
+    PSI_variablePSI = PSI_variablePSI[startIndex:endIndex]
+    plt.plot(t, PSI_variablePSI, '#62da17', linewidth=1, label='Variable PSI', linestyle='-')
 
-    # PSI_ConstantPSI = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-PSISet-" + str(1) + ".txt")
-    # PSI_ConstantPSI = PSI_ConstantPSI[startIndex:endIndex]
-    # plt.plot(t, PSI_ConstantPSI, '#8f17da', linewidth=1, label='Constant PSI', linestyle='-')
+    PSI_ConstantPSI = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-PSISet-" + str(1) + ".txt")
+    PSI_ConstantPSI = PSI_ConstantPSI[startIndex:endIndex]
+    plt.plot(t, PSI_ConstantPSI, '#8f17da', linewidth=1, label='Constant PSI', linestyle='-')
 
-    # plt.legend(loc='lower left')
-    # plt.ylim(0, 1.1)
-    # plt.ylabel('PSI')
+    plt.legend(loc='lower left')
+    plt.ylim(-0.2, 1.2)
+    plt.ylabel('PSI')
+    plt.xlim(-0.5, 20.5)
+
+    plt.text(10, -0.78, '(c)', horizontalalignment='center', verticalalignment='center', fontsize=11)
+    plt.text(10, -1.08, 'time (s)', horizontalalignment='center', verticalalignment='center', fontsize=11)
+
     # plt.xlabel('time (s)')
-    # # plt.show()
+    # plt.show()
 
     # plt.savefig('my_project\scripts\pic\\' + "PSI" + '.pdf')
 
     #=========================================================# # 画交互力图——方案对比
-    # human = 5
-    # times = 1
-    # forcePrinter = ForcePrinter(600, 3.7) 
-    # # plt.subplot(3, 1, 2)
-    # # force = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-allHuman-forceSet-" + str(times) + ".txt")
-    # # forcePrinter.loadForce(force, 'orange', 'master-slave')
-    # # forcePrinter.loadBackgroud(82, 179)
-    # # force1Sum = np.sum(force[82:179])
-    # # forcePrinter.loadBackgroud(261, 546)
-    # # force2Sum = np.sum(force[261:546])
-    # # plt.ylabel('force (N)')
-    # # plt.ylim(-0.2, 5.5)
+    human = 6
+    times = 1
+    forcePrinter = ForcePrinter(600, 3.7) 
+    plt.subplot(3, 1, 2)
+    force = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-allHuman-forceSet-" + str(times) + ".txt")
+    forcePrinter.loadForce(force, 'orange', 'master-slave')
+    forcePrinter.loadBackgroud(82, 179)
+    force1Sum = np.sum(force[82:179])
+    forcePrinter.loadBackgroud(261, 546)
+    force2Sum = np.sum(force[261:546])
+    plt.ylabel('force (N)')
+    plt.ylim(-0.2, 5.5)
+    plt.xlim(-2, 62)
 
-    # # plt.legend(loc='right')
+    plt.legend(loc='right')
+    
+    plt.text(30, -2.5, '(b)', horizontalalignment='center', verticalalignment='center', fontsize=11)
 
-    # # plt.subplot(3, 1, 3)
-    # force = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-forceSet-" + str(times) + ".txt")
-    # forcePrinter.loadForce(force, 'b', 'shared control')
-    # forcePrinter.loadThreshold()
-    # # forcePrinter.loadBackgroud(81, 153)
-    # # force1Sum_share = np.sum(force[81:153])
-    # # forcePrinter.loadBackgroud(213, 471)
-    # # force2Sum_share = np.sum(force[213:471])
-    # # plt.legend(loc='right')
-
-    # forcePrinter.loadChangePoint(131)
-    # forcePrinter.loadChangePoint(201)
-    # forcePrinter.loadChangePoint(231)
-
-    # # print("1:", abs(force1Sum_share - force1Sum) / force1Sum) # 21.51%
-    # # print("2:", abs(force2Sum_share - force2Sum) / force2Sum) # 52.83%
-
-    # # 设置y轴范围
-    # plt.ylim(-0.2, 5.5)
-    # # 设置x名称
+    plt.subplot(3, 1, 3)
+    force = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-forceSet-" + str(times) + ".txt")
+    forcePrinter.loadForce(force, 'b', 'shared control')
+    forcePrinter.loadThreshold()
+    forcePrinter.loadBackgroud(81, 153)
+    force1Sum_share = np.sum(force[81:153])
+    forcePrinter.loadBackgroud(213, 471)
+    force2Sum_share = np.sum(force[213:471])
+    plt.legend(loc='right')
+    # 设置y轴范围
+    plt.ylim(-0.2, 5.5)
+    # 设置x名称
     # plt.xlabel('time (s)')
-    # # 设置y名称
-    # plt.ylabel('force (N)')
+    # 设置y名称
+    plt.ylabel('force (N)')
+    plt.xlim(-2, 62)
 
-    # # force = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-forceSet-" + str(times) + ".txt")
-    # # forcePrinter.loadForce(force, 'b', 'shared control')
+    plt.subplots_adjust(bottom=0.2)
 
-    # changeIndex = [131, 201, 231]
-    # for index in changeIndex:
-    #     forcePrinter.loadChangePoint(index)
+    plt.text(30, -2.5, '(c)', horizontalalignment='center', verticalalignment='center', fontsize=11)
 
-    # # forcePrinter.print()
+    plt.text(30, -3.8, 'time (s)', horizontalalignment='center', verticalalignment='center', fontsize=11)
+
+    forcePrinter.loadChangePoint(131)
+    forcePrinter.loadChangePoint(201)
+    forcePrinter.loadChangePoint(231)
+
+    print("force sum:", force1Sum, force2Sum, force1Sum_share, force2Sum_share)
+    print("1:", abs(force1Sum_share - force1Sum) / force1Sum) # 21.51%
+    print("2:", abs(force2Sum_share - force2Sum) / force2Sum) # 52.83%
+
+
+    force = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-forceSet-" + str(times) + ".txt")
+    forcePrinter.loadForce(force, 'b', 'shared control')
+
+    changeIndex = [131, 201, 231]
+    for index in changeIndex:
+        forcePrinter.loadChangePoint(index)
+
+    forcePrinter.print()
     # forcePrinter.save("replan-force")
 
     #=========================================================# # 画交互力图——对比一致性
@@ -533,6 +563,8 @@ if __name__ == "__main__":
     #=========================================================# # 计算兴趣点折线图
     # human = 6
     # times = 1
+    # forcePrinter = ForcePrinter(600, 3.7) 
+
     # intrestingPointSet = np.loadtxt("my_project\scripts\Data\intrestingPoint.txt")
     # humanTrajectory = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-allHuman-realTraj-1.txt")
     # replanTrajectory = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-realTraj-1.txt")
@@ -546,7 +578,7 @@ if __name__ == "__main__":
     # # fig = plt.figure()
     # plt.plot(humanErr * 1000, 'orange', linewidth=1, label='master-slave', linestyle='-')
     # plt.plot(shareErr * 1000, 'b', linewidth=1, label='shared control', linestyle='-')
-    # plt.legend(loc='upper left')
+    # plt.legend(loc='upper left', ncol = 1)
     # plt.ylabel('error (mm)')
 
     # # 绘制点
@@ -554,9 +586,57 @@ if __name__ == "__main__":
     # plt.plot(pointIndex, humanErr[pointIndex] * 1000, 'orange', marker='.', markersize=5)
     # plt.plot(pointIndex, shareErr[pointIndex] * 1000, 'b', marker='.', markersize=5)
 
-    # plt.ylim(-0.2, 5)
+    # # 设置y轴范围,分度值为5, 显示0、5、10，但是从-0.2开始
+    # plt.ylim(-0.2, 10.2)
+    # plt.yticks(np.arange(0, 10.1, 5))
     # # 设置x轴显示1，2，3，4
     # plt.xticks(pointIndex, ('Point1', 'Point2', 'Point3', 'Point4'))
+
+    # # 图表下方插入
+    # plt.subplots_adjust(hspace=0.55)
+
+    # # 图表下方插入（a）
+    # plt.text(1.5, -3.6, '(a)', horizontalalignment='center', verticalalignment='center', fontsize=11)
+
+    # plt.subplot(3, 1, 2)
+    # force = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-allHuman-forceSet-" + str(times) + ".txt")
+    # forcePrinter.loadForce(force, 'orange', 'master-slave')
+    # forcePrinter.loadBackgroud(82, 179)
+    # force1Sum = np.sum(force[82:179])
+    # forcePrinter.loadBackgroud(261, 546)
+    # force2Sum = np.sum(force[261:546])
+    # plt.ylabel('force (N)')
+    # plt.ylim(-0.2, 5.5)
+    # plt.yticks(np.arange(0, 5.6, 2.5))
+    # plt.xlim(-2, 62)
+
+    # plt.legend(loc='lower right')
+    
+    # plt.text(30, -2.5, '(b)', horizontalalignment='center', verticalalignment='center', fontsize=11)
+
+    # plt.subplot(3, 1, 3)
+    # force = np.loadtxt("my_project\scripts\Data\\" + str(human) + "-replan-forceSet-" + str(times) + ".txt")
+    # forcePrinter.loadForce(force, 'b', 'shared control')
+    # forcePrinter.loadThreshold()
+    # forcePrinter.loadBackgroud(81, 153)
+    # force1Sum_share = np.sum(force[81:153])
+    # forcePrinter.loadBackgroud(213, 471)
+    # force2Sum_share = np.sum(force[213:471])
+    # plt.legend(loc='lower right')
+    # # 设置y轴范围
+    # plt.ylim(-0.2, 5.5)
+    # plt.yticks(np.arange(0, 5.6, 2.5))
+    # # 设置x名称
+    # # plt.xlabel('time (s)')
+    # # 设置y名称
+    # plt.ylabel('force (N)')
+    # plt.xlim(-2, 62)
+
+    # plt.subplots_adjust(bottom=0.2)
+
+    # plt.text(30, -2.5, '(c)', horizontalalignment='center', verticalalignment='center', fontsize=11)
+
+    # plt.text(30, -3.8, 'time (s)', horizontalalignment='center', verticalalignment='center', fontsize=11)
 
     # # plt.show()
     # plt.savefig("my_project\scripts\pic\\" + "contrast" + '.pdf')
@@ -627,8 +707,8 @@ if __name__ == "__main__":
     # width = 0.1  # the width of the bars
     # fig, ax = plt.subplots()    
     
-    # ax.bar(x - width/2, humanMean, width, yerr=humanVar, label='human leading', error_kw=dict(ecolor='gray', lw=1, capsize=3, capthick=1))
-    # ax.bar(x + width/2, sharedMean, width, yerr=sharedVar, label='shared control', error_kw=dict(ecolor='gray', lw=1, capsize=3, capthick=1))
+    # ax.bar(x - width/2, humanMean, width, label='human leading', error_kw=dict(ecolor='gray', lw=1, capsize=3, capthick=1))
+    # ax.bar(x + width/2, sharedMean, width, label='shared control', error_kw=dict(ecolor='gray', lw=1, capsize=3, capthick=1))
 
     # # Add some text for labels, title and custom x-axis tick labels, etc.
     # ax.set_ylabel('error (m)')
@@ -648,7 +728,7 @@ if __name__ == "__main__":
 
     # print([F_Point1, F_Point2, F_Point3, F_Point4])
     
-    #=========================================================# # 交互力的方差分析
+    # #=========================================================# # 交互力的方差分析
     # controllerFreq = 10 
     # avrSpeed = 0.01
     # distance = 0.1
